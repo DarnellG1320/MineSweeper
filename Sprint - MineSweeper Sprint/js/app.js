c1760606d120013d5e0eaf5619807c194c7d24a5
DarnellG1320 <114085161+DarnellG1320@users.noreply.github.com>
2022-09-22 20:09:46 +0300

    Fixed bug in Best Score

---
+++ Sprint - MineSweeper Sprint/js/app.js
@@ -0,0 +1,327 @@
+'use strict';
+
+const TILE = 'WALL';
+const FLOOR = 'FLOOR';
+
+var BOMB = ' ';
+
+const TILE_STATUSES = {
+  HIDDEN: 'hidden',
+  MINE: 'mine',
+  NUMBER: 'number',
+  MARKED: 'marked',
+};
+
+var gCollectedBOMBCount = 0;
+var gTotalTilesCount = 0;
+var gCellClicked = false;
+var gIsFirstClick = true;
+var gisGameLost = false;
+var gGameIsPlaying = false;
+var gLivesCount = 3;
+var gBestScore = 0;
+var scoreToWin = 0;
+
+var gTotalBOMBCount = 2;
+var SIZE = 8;
+
+var gIntervalId = null;
+
+var gSelectedElCell;
+
+var gIsRightClick = false;
+
+var gGameScore = 0;
+var gBoard;
+
+//  ****** right click events *****
+
+window.oncontextmenu = function () {
+  rightClicked();
+  return false; // cancel default menu
+};
+
+function rightClicked() {
+  // gIsRightClick = !gIsRightClick;
+  alert('Yo');
+  // cellClicked()
+}
+
+//**** Local Storage *****
+
+var storedBestScore = localStorage.getItem('Best Score', gGameScore);
+
+var bestScore = document.getElementById('bestScore');
+
+bestScore.innerHTML = null;
+bestScore.innerHTML += `${'Best Score'}: ${storedBestScore}`;
+var key = localStorage.getItem('Name', key);
+// **** DOM name field and input field ****
+
+var inputKey = document.getElementById('inputKey');
+var btnInsert = document.getElementById('btnInsert');
+var isNamed = document.getElementById('isNamed');
+
+isNamed.innerHTML += `${'Player Name'}: ${key}`;
+// bestScore.innerHTML += `${'Best Score'}: ${gBestScore}`;
+btnInsert.onclick = function () {
+  var key = inputKey.value;
+
+  if (key) {
+    localStorage.setItem('Name', key);
+  }
+  isNamed.innerHTML = null;
+  isNamed.innerHTML += `${'Player Name'}: ${key}`;
+  console.log(localStorage);
+};
+
+function changeFunc() {
+  var selectBox = document.getElementById('selectBox');
+  SIZE = +selectBox.options[selectBox.selectedIndex].value;
+  scoreToWin = 0;
+  initGame();
+}
+
+function restartGame() {
+  gGameScore = 0;
+  scoreToWin = 0;
+  initGame();
+  updateScore(0);
+}
+
+function initGame(SIZE = 12) {
+  // var lastBestScore = localStorage.getItem('Best Score');
+  // document.querySelector('h2 span').innerText = lastBestScore;
+  console.log('localStorage: ', localStorage);
+
+  clearInterval(gIntervalId);
+  gGameScore = 0;
+  gLivesCount = 3;
+  gBestScore = 0;
+  gIsFirstClick = true;
+  gisGameLost = false;
+
+  gBoard = buildBoard(SIZE);
+  console.log('gBoard: ', gBoard);
+  renderBoard(gBoard);
+
+  // scoreToWin = calculateTileAmt()
+  hideTiles(gBoard);
+  // console.log('scoreToWin: ', scoreToWin);
+  clearInterval(gIntervalId);
+}
+
+function buildBoard() {
+  clearInterval(gIntervalId);
+  var board = [];
+
+  // Create the Matrix
+  board = createMat(SIZE, SIZE);
+
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      board[i][j] = { i, j, type: TILE, gameElement: null, bombCount: null };
+      gTotalTilesCount++;
+    }
+  }
+
+  //********* MINE ADDER ******
+
+  var totalBOMBCount = convertSizeToBombAmt(SIZE);
+
+  for (var k = 0; k < totalBOMBCount; k++) {
+    board[getRandomInt(0, SIZE)][getRandomInt(0, SIZE)].gameElement = BOMB;
+  }
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[i].length; j++) {
+      var currCellBombsCount = countBombsAround(board, i, j);
+
+      board[i][j].bombCount = currCellBombsCount;
+    }
+  }
+  console.log('board: ', board);
+
+  return board;
+}
+
+function convertSizeToBombAmt(SIZE) {
+  if (SIZE === 4) {
+    SIZE = 2;
+  } else if (SIZE === 8) {
+    SIZE = 14;
+  } else if (SIZE === 12) {
+    SIZE = 32;
+  } else if (SIZE === 16) {
+    SIZE = 64;
+  }
+  return SIZE;
+}
+
+function renderBoard(board) {
+  var elBoard = document.querySelector('.board');
+  var strHTML = '';
+
+  for (var i = 0; i < board.length; i++) {
+    strHTML += '<tr>\n';
+
+    for (var j = 0; j < board[0].length; j++) {
+      var currCell = board[i][j];
+
+      var cellClass = getClassName({ i, j });
+
+      // if cellClicked - remove status hidden
+      currCell.status = 'HIDDEN';
+
+      if (currCell.status === 'HIDDEN')
+        if (currCell.type === FLOOR) cellClass += ' floor';
+        else if (currCell.type === TILE) cellClass += ' wall';
+      //prettier-ignore
+      strHTML += `\t<td data-type="status" class="cell ${cellClass}" 
+      onclick="cellClicked(this, event, ${i}, ${j})" >`;
+
+      if (currCell.gameElement === BOMB && gCellClicked) {
+      } else if (currCell.gameElement === BOMB) {
+        strHTML += BOMB;
+      } else if (currCell.gameElement === '💥' && gisGameLost) {
+        strHTML += '💥';
+      }
+
+      strHTML += '\t</td>\n';
+    }
+    strHTML += '</tr>\n';
+  }
+
+  // console.log('strHTML is:');
+  // console.log(strHTML);
+  elBoard.innerHTML = strHTML;
+}
+
+function showBOMBS(board) {
+  var currCell;
+
+  for (var i = 0; i < SIZE; i++) {
+    for (var j = 0; j < SIZE; j++) {
+      currCell = board[i][j];
+
+      if (currCell.gameElement === ' ') {
+        currCell.gameElement = '💥';
+        console.log('currCell.gameElement: ', currCell.gameElement);
+      }
+    }
+  }
+}
+
+function showZeros(board) {
+  var currCell;
+
+  for (var i = 0; i < SIZE; i++) {
+    for (var j = 0; j < SIZE; j++) {
+      currCell = board[i][j];
+
+      if (currCell.bombCount === 0) {
+        currCell.gameElement += '🌺';
+      }
+    }
+  }
+}
+function checkWin() {
+  if (gGameScore !== gTotalTilesCount) {
+    // console.log('gTotalTilesCount: ', gTotalTilesCount);
+    // console.log('gGameScore: ', gGameScore);
+    winOrLose(gGameScore);
+  }
+}
+
+function winOrLose() {
+  var storedBestScore = localStorage.getItem('Best Score', gBestScore);
+  // console.log('storedBestScore: ', storedBestScore);
+  if (storedBestScore > gGameScore) return;
+  else localStorage.setItem('Best Score', gGameScore);
+  bestScore.innerHTML = null;
+  bestScore.innerHTML += `${'Best Score'}: ${gGameScore}`;
+}
+
+function hideTiles(board) {
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[0].length; j++) {
+      var currCell = board[i][j];
+
+      if (currCell.status === 'hidden') return;
+    }
+  }
+}
+
+function cellClicked(elCell, event, i, j) {
+  checkWin();
+  if (gisGameLost) return;
+  // showZeros(gBoard)
+  renderLives();
+
+  if (gIsFirstClick) {
+    var isFirstClick = true;
+    gIsFirstClick = false;
+  } else {
+    isFirstClick = false;
+  }
+
+  if (!gGameIsPlaying) {
+    startTimer();
+    winOrLose();
+    gGameIsPlaying = true;
+  } else if (gisGameLost) return;
+  elCell.style.backgroundColor = 'rgb(224, 117, 117)';
+  if (event.type === 'click') var BOMBCount = countBombsAround(gBoard, i, j);
+
+  if (gBoard[i][j].gameElement !== BOMB);
+  {
+    elCell.innerText = BOMBCount;
+    showZeros(gBoard);
+    // renderBoard(gBoard)
+    updateScore(1);
+  }
+  for (var k = 0; k < SIZE; k++) {
+    for (var p = 0; p < SIZE; p++) {
+      if (gBoard[i][j].gameElement === BOMB && gBoard[i][j].bombCount === 0)
+        elCell.innerText = BOMBCount;
+    }
+  }
+  // console.log('BOMBCount: ', BOMBCount);
+
+  /// ***** remove hidden *******
+
+  gCellClicked = !gCellClicked;
+  if (gBoard[i][j].status === 'HIDDEN')
+    if (gBoard[i][j].status === 'marked') return;
+  if (gBoard[i][j].status !== 'marked') {
+    gBoard[i][j].status = 'marked';
+  }
+
+  elCell.classList.add('selected');
+  gSelectedElCell = elCell;
+  // console.log('gSelectedElCell: ', gSelectedElCell);
+
+  if (isFirstClick && gBoard[i][j].gameElement === BOMB) {
+    elCell.innerText = 'U Lucky';
+    isFirstClick = false;
+  } else if (gBoard[i][j].gameElement === BOMB) {
+    elCell.innerText = '💥';
+    gLivesCount--;
+    renderLives();
+    // winOrLose(gGameScore)
+
+    //********** BOMB HIT EVENTS *********
+  }
+  if (gLivesCount === 0) {
+    gisGameLost = true;
+    gGameIsPlaying = false;
+    showBOMBS(gBoard);
+    clearInterval(gIntervalId);
+    renderLives();
+    renderBoard(gBoard);
+    // showBestScore()
+    alert('Game Over');
+  }
+  var cellCoord = getCellCoord(elCell.id);
+  markCells(cellCoord);
+  return true;
+}
---
+++ Sprint - MineSweeper Sprint/js/util.js
@@ -0,0 +1,149 @@
+function createMat(ROWS, COLS) {
+  var mat = [];
+  for (var i = 0; i < ROWS; i++) {
+    var row = [];
+    for (var j = 0; j < COLS; j++) {
+      row.push('');
+    }
+    mat.push(row);
+  }
+  return mat;
+}
+
+// function getEmptyPos() {
+//   var possibleCells = [];
+//   for (var i = 0; i < gBoard.length; i++) {
+//     for (var j = 0; j < gBoard[i].length; j++) {
+//       var currCell = gBoard[i][j];
+//       if (currCell.type !== WALL && !currCell.gameElement) {
+//         possibleCells.push({ i, j });
+//       }
+//     }
+//   }
+//   return possibleCells;
+// }
+
+// Returns the class name for a specific cell
+function getClassName(location) {
+  var cellClass = 'cell-' + location.i + '-' + location.j;
+  return cellClass;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+function startTimer() {
+  clearInterval(gIntervalId);
+  var startTime = Date.now();
+  gIntervalId = setInterval(function () {
+    var elTime = document.querySelector('.timePlayed');
+    elTime.innerText = 'Time Played:' + '  ';
+    var secs = ((Date.now() - startTime) / 1000).toFixed(0);
+    if (secs < 90) {
+      elTime.innerText += '0';
+      elTime.innerText += secs;
+    } else if (secs < 9) {
+      elTime.innerText += '0';
+      elTime.innerText += secs;
+    } else {
+      elTime.innerText += secs;
+    }
+  }, 10);
+}
+
+
+// Convert a location object {i, j} to a selector and render a value in that element
+function renderCell(location, value) {
+  var cellSelector = '.' + getClassName(location);
+  var elCell = document.querySelector(cellSelector);
+  elCell.innerHTML = value;
+}
+
+function updateScore(score) {
+  gBestScore++
+  gGameScore += score;
+  
+  document.querySelector('h2 span').innerText = gGameScore;
+}
+
+function countBombsAround(board, rowIdx, colIdx) {
+  var BOMBCount = 0;
+
+  for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+    if (i < 0 || i >= SIZE) continue;
+
+    for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+      if (j < 0 || j >= SIZE) continue;
+      if (i === rowIdx && j === colIdx) continue;
+
+      var currCell = board[i][j];
+      if (currCell.gameElement === BOMB) BOMBCount++;
+    }
+  }
+  return BOMBCount;
+}
+
+
+// function countZerosAround(board, rowIdx, colIdx) {
+  
+
+//   for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+//     if (i < 0 || i >= SIZE) continue;
+
+//     for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+//       if (j < 0 || j >= SIZE) continue;
+//       if (i === rowIdx && j === colIdx) continue;
+
+//       var currZeroCell = board[i][j];
+//        currZeroCellArr.push(currZeroCell)
+
+//       console.log('currCell: ', currCell);
+//       if (currZeroCell.gameElement !== BOMB) BOMBCount++;
+//     }
+//   }
+//   return currZeroCellArr;
+// }
+
+
+function markCells(coords) {
+  // query select them one by one and add mark
+  for (var i = 0; i < coords.length; i++) {
+    // #cell-3-2
+    var selector = getSelector(coords[i]);
+    console.log('selector: ', selector);
+    var elCell = document.querySelector(selector);
+    elCell.classList.add('mark');
+  }
+}
+
+function getSelector(coord) {
+  return `#cell-${coord.i}-${coord.j}`;
+}
+
+function getCellCoord(strCellId) {
+  var coord = {};
+  var parts = strCellId.split('-');
+  coord.i = +parts[1];
+  coord.j = +parts[2];
+  return coord;
+}
+
+
+function renderLives(){
+  var livesCount = document.getElementById('livesCount')
+  livesCount.innerHTML = null;
+  livesCount.innerHTML += `${'Lives Left'}: ${gLivesCount}`;
+
+}
+
+function calculateTileAmt() {
+  var tilesSum = 0
+  // debugger
+ tilesSum = (gTotalTilesCount - gTotalBOMBCount )
+ console.log('tilesSum: ', tilesSum);
+ return tilesSum
+}
